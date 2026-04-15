69febeabc4c883548e0afdd9a956d8397204d930
Arvind Kumar <arvindkrbscc2006@gmail.com>
2026-04-15 06:01:54 -1200

    Update admin-middleware.js

--- Backend/Middlewares/admin-middleware.js
+++ Backend/Middlewares/admin-middleware.js
@@ -25,18 +25,3 @@ module.exports = adminMiddleware;
 
 
 
-// const adminMiddleware = async (req, res, next) => {
-//     try {
-       
-//         const adminRole = req.user.isAdmin;
-//         if (!adminRole) {
-//             return res.status(403).json({ message: "Access denied. User is not Admin" });
-//         }
-        
-//         next();
-//     } catch (error) {
-//         next(error);
-
-//     }
-// };
-// module.exports = adminMiddleware;
